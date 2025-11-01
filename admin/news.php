5ed01a73d8bcf5dc7177e4f09d090c67525acdff
Mahmoud <your@email.com>
2025-11-01 23:44:12 +0200

    add news

--- admin/news.php
+++ admin/news.php
@@ -52,15 +52,15 @@ if (!defined('IS_ADMIN_PANEL')) {
   </div>
 
   <div class="AddNews_Con">
-    <div class="AddNews">
+    <form class="AddNews" action="/Municipality/backend/add_news.php" method="post" enctype="multipart/form-data">
       <h1>Add News</h1>
-      <input type="text" placeholder="Title" required maxlength="150" />
-      <textarea placeholder="Description" required maxlength="1000"></textarea>
+      <input type="text" name="title"  placeholder="Title" required maxlength="150" />
+      <textarea placeholder="Description" name="description" required maxlength="1000"></textarea>
 
       <div class="visibility_con">
         <p>Visibility</p>
         <div class="form_control">
-          <select required>
+          <select name="visibility"  required>
             <option value="0">Private</option>
             <option value="1">Public</option>
           </select>
@@ -74,13 +74,13 @@ if (!defined('IS_ADMIN_PANEL')) {
   <h3>Upload Image</h3>
   <p>Drag & drop an image here or click to select</p>
   <label for="file">Upload</label>
-  <input type="file" accept="image/*" hidden id="file" />
+  <input name="image" type="file" accept="image/*" hidden id="file" />
 </div>
 
       </div>
 
-      <button class="publish_news">Publish</button>
-    </div>
+      <button type="submit"  class="publish_news">Publish</button>
+    </form>
   </div>
 
 
